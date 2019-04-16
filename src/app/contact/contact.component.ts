5091943ed9a1ffa6147b85299a1b0153c8cadad4
Piotr <augustyniak.p@gmail.com>
2019-04-16 20:30:05 +0200

    working on form validation and consent

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,11 +9,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ContactComponent implements OnInit {
 	latitude = 51.1361514;
 	longitude = 17.050906;
+	public show:boolean = false;
 	isContactPage: boolean = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
 		this.isContactPage = this.router.url ==='/kontakt' ? true : false;
   }
+
+	toggleConsent() {
+		this.show = !this.show;
+	}
 
 }
